b5815353885287864cf4d1f0bdb33c5b062ed29e
FalatM <73362691+FalatM@users.noreply.github.com>
2021-06-12 20:59:18 -0400

    Removed menu icon and function. This Is a basic checklist for clearning cars for customers. Maybe will add menu back with other features.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,15 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
   },
 }));
 
@@ -22,9 +18,6 @@ export default function Navbar() {
     <div className={classes.root}>
       <AppBar position="static" style={{ background: '#169A5A' }}>
         <Toolbar variant="dense">
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" color="inherit">
             Clean Car Check List
           </Typography>
